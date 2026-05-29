c6cc8a7334bbfd9b32a2206d5c4320ffa7f0a2eb
Firecx <nikandrov.alekseix1@gmail.com>
2026-05-29 21:54:48 +0600

    fix

--- proxyrasp/src/main/java/ru/neomgtu/proxyrasp/controller/ScheduleController.java
+++ proxyrasp/src/main/java/ru/neomgtu/proxyrasp/controller/ScheduleController.java
@@ -2,11 +2,13 @@ package ru.neomgtu.proxyrasp.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -40,6 +42,7 @@ public class ScheduleController {
     };
 
     @PostMapping(value = "/group/{groupId}/save", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
     public Mono<Void> saveGroupSchedule(
             @PathVariable String groupId,
             @RequestParam String start,
